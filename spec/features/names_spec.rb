93c0a87a3318081e788fb57cd61e45edeb4c743c
kpuwal <kpuwal@gmail.com>
2016-06-08 16:34:52 +0100

    battle: atacking player

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,23 +1,32 @@
 require 'capybara/dsl'
-require 'spec_helper'
+#require 'spec_helper'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    sign_in_and_play
+    visit('/')
+  fill_in :player_1_name, with: 'Dave'
+  fill_in :player_2_name, with: 'Mittens'
+  click_button 'Submit'
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
 
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    sign_in_and_play
+    visit('/')
+  fill_in :player_1_name, with: 'Dave'
+  fill_in :player_2_name, with: 'Mittens'
+  click_button 'Submit'
     expect(page).to have_content 'Mittens: 60HP'
   end
 end
 
 feature 'Attacking' do
   scenario 'attack Player 2' do
-    sign_in_and_play
+    visit('/')
+  fill_in :player_1_name, with: 'Dave'
+  fill_in :player_2_name, with: 'Mittens'
+  click_button 'Submit'
     click_button 'Attack'
     expect(page).to have_content 'Dave attacked Mittens'
   end
